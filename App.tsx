17c821dfb3426664a42563d052dc63a6a3109695
littxx <ederlanbrito@gmail.com>
2023-08-25 12:02:57 -0300

    bora

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text, View} from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <Routes />
+      <View>
+        <Text>ola</Text>
+      </View>
     </SafeAreaView>
   );
 };
